9c9e14cc5120f2bd90799307ebf09166f46f96a6
Ryan Brooks <ryan@slatehorse.com>
2018-01-09 09:51:19 +0000

    initial commit

---
+++ ckanext/string_to_location/plugin.py
@@ -0,0 +1,13 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class String_To_LocationPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'string_to_location')
