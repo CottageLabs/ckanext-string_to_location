15184dbb4b658bc9f49be9bb7165f886f9eb0130
Ryan Brooks <ryan@slatehorse.com>
2018-01-24 14:00:35 +0000

    requirements aren't included in dev-requirements

--- bin/travis-build-plugin.bash
+++ bin/travis-build-plugin.bash
@@ -2,7 +2,7 @@ echo "This is travis-build-deps.bash..."
 
 echo "Installing ckanext-string_to_location and its requirements..."
 python setup.py develop
-pip install -r dev-requirements.txt
+pip install -r requirements.txt
 
 echo "Moving test.ini into a subdir..."
 mkdir subdir
