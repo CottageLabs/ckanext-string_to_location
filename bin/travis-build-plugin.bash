db4e184f1b997721180b0fee4c7c59739746f27e
Ryan Brooks <ryan@slatehorse.com>
2018-01-24 14:08:57 +0000

    upgrade psyco to fix https://github.com/psycopg/psycopg2/issues/594

--- bin/travis-build-plugin.bash
+++ bin/travis-build-plugin.bash
@@ -3,6 +3,7 @@ echo "This is travis-build-deps.bash..."
 echo "Installing ckanext-string_to_location and its requirements..."
 python setup.py develop
 pip install -r requirements.txt
+pip install -r dev-requirements.txt --upgrade
 
 echo "Moving test.ini into a subdir..."
 mkdir subdir
