3cedc9b0cd24360a0b7b57b1c80bd9477f8db590
Nevelina Aleksandrova <nevelina@cottagelabs.com>
2018-01-30 11:33:25 +0000

    We don't need to match

--- ckanext/string_to_location/location_mapper.py
+++ ckanext/string_to_location/location_mapper.py
@@ -11,7 +11,6 @@ from StringIO import StringIO
 from ckanext.string_to_location.ons_entity_types import OnsEntityTypes
 from ckanext.string_to_location.null_ons_entity import NullOnsEntity
 from ckanext.string_to_location.ons_entity_builder import OnsEntityBuilder
-from ckanext.string_to_location.ons_code_mapper import OnsCodeMapper
 
 
 class LocationMapper:
@@ -30,11 +29,10 @@ class LocationMapper:
     def map_location(self):
                        
         source_entity_type = self.COLUMN_TYPE_TO_ENTITY_TYPE[self.column_type]
-        target_entity_type = OnsEntityTypes.LOCAL_AUTHORITY_DISTRICT
        
-        matches, errors = self._build_matches(self.table, self.column_name, self.is_name, source_entity_type, target_entity_type)
+        entities, errors = self._build_entities(self.table, self.column_name, self.is_name, source_entity_type)
 
-        geojson_version = self._matches_to_geojson(matches, list(self.table.columns))
+        geojson_version = self._entities_to_geojson(entities, list(self.table.columns))
 
         output_buffer = StringIO()
         geojson.dump(geojson_version, output_buffer, ignore_nan=True)
@@ -60,50 +58,39 @@ class LocationMapper:
         # print(f"    {matches_with_polygons} matches with polygons")
         
 
-    def _matches_to_geojson(self, match_array, properties):
+    def _entities_to_geojson(self, entities_array, properties):
             features = []
-            for match in match_array:
-                geometry = match['entity'].geo_polygon.geometry
+            for entity in entities_array:
+                geometry = entity['entity'].geo_polygon.geometry
                 feature_properties = {}
                 for prop in properties:
-                    feature_properties[prop] = match['row'][prop]
+                    feature_properties[prop] = entity['row'][prop]
                 feature = Feature(properties= feature_properties, geometry=geometry)
                 features.append(feature)
 
             return FeatureCollection(features)
 
-    def _build_matches(self, table, column_name, is_name, source_entity_type, target_entity_type):
-        # Build the matches array
-
-        # Attempt to map this from a CSP code into additional entities
-        sources = []
-        matches = []
+    def _build_entities(self, table, column_name, is_name, source_entity_type):
+        entities = []
         errors = []
         rows = 0
         for index, row in table.iterrows():
             rows += 1
-            # For CSP lookup
             lookup_name = row[column_name]
-            # First, build the ONS entity, so we can pass that around consistently
-            # FIXME: this shouldn't be an if statement
+            
             ons_entity = OnsEntityBuilder.build(lookup_name, source_entity_type, is_name=is_name)
 
-            if not isinstance(ons_entity, NullOnsEntity):
-                sources.append(ons_entity)
-                row[source_entity_type.value + '_geojson'] = ons_entity.geo_polygon
-
-            # Second, convert the entity into a LAD (builds a new object, of course)
-            local_authority_district = OnsCodeMapper(ons_entity, target_entity_type).call()
-
-            if isinstance(local_authority_district, NullOnsEntity):
+            # FIXME: this shouldn't be an if statement
+            if isinstance(ons_entity, NullOnsEntity):
                 error_message = "Row:" + str(index) + ", " + lookup_name + " did not match."
                 errors.append(error_message)
             else:
-                matches.append({
-                    'entity': local_authority_district,
+                row[source_entity_type.value + '_geojson'] = ons_entity.geo_polygon
+                entities.append({
+                    'entity': ons_entity,
                     'row': row
                 })
 
-        return matches, errors
+        return entities, errors
 
     
