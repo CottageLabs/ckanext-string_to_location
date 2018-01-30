74f06c99eb1d3d2c42a4cc7fa948f78f14db0383
Ryan Brooks <ryan@slatehorse.com>
2018-01-30 18:20:05 +0000

    🛀 Refactor for cleanliness and clarity 🛁

--- ckanext/string_to_location/location_mapper_job.py
+++ ckanext/string_to_location/location_mapper_job.py
@@ -21,59 +21,46 @@ def perform(resource_id, column_name, column_type, is_name, username):
     '''
     Background job
     '''
+    context = None
+    resource = toolkit.get_action('resource_show')(context, {'id': resource_id})
+    log_writer = LocationMapperLogWriter(resource['id'])
+
     try:
-        context = None
-        resource = toolkit.get_action('resource_show')(context, {'id': resource_id})
+        table = __read_csv_from_resource(resource)
 
-        log_writer = LocationMapperLogWriter(resource['id'])
-
-        resource_path = uploader.get_resource_uploader(resource).get_path(resource['id'])
-        resource_contents = codecs.open(resource_path, 'rb', 'cp1257')
-
-        table = pandas.read_csv(resource_contents)
         row_count = table.shape[0]
         column_count = table.shape[1]
         log_writer.info("Loaded resource contents: " + str(row_count) + " rows, " + str(column_count) + " columns")
 
-        try:
-            geojson_version = LocationMapper(table, column_name, column_type, is_name).map_and_build_geojson()
+        # This throws a LookupNameMissingException if the column doesn't exist in the resource
+        geojson_version = LocationMapper(table, column_name, column_type, is_name).map_and_build_geojson()
 
-            match_percentage = len(geojson_version.features) / float(row_count)
-            log_writer.info("Matched ONS GeoJSON objects: {} of {} ({:.1%})".format(len(geojson_version.features), row_count, match_percentage))
+        match_percentage = len(geojson_version.features) / float(row_count)
+        log_writer.info("Matched ONS GeoJSON objects: {} of {} ({:.1%})".format(len(geojson_version.features), row_count, match_percentage))
 
-            output_buffer = StringIO()
-            geojson.dump(geojson_version, output_buffer, ignore_nan=True)
+        new_resource = __create_and_upload_geojson_resource(package_id=resource['package_id'],
+                                                    name="Augmented " + resource['name'],
+                                                    description="Geo-mapped representation of " + resource['name'],
+                                                    filename='mapped_output.geojson',
+                                                    geojson_obj=geojson_version,
+                                                    username=username)
 
-            upload = cgi.FieldStorage()
-            # FIXME: replace with source filename + modified extension
-            upload.filename = 'mapped_output.geojson'
-            upload.file = output_buffer
+        # Update the user-facing log
+        log_writer.info("Added new resource to dataset {}{}".format(
+                            config.get('ckan.site_url'),
+                            helpers.url_for(controller='package',
+                                            action='resource_read',
+                                            id=new_resource['package_id'],
+                                            resource_id=new_resource['id'])))
 
-            data_dict = {
-                "package_id": resource['package_id'],
-                # TODO improve naming
-                "name": "Augmented " + resource['name'],
-                # TODO improve description
-                "description": "Geo-mapped representation of " + resource['name'],
-                "format": "application/geo+json",
-                "upload": upload
-            }
+        log_writer.info("Location mapping completed", state="complete")
 
-            new_resource = logic.action.create.resource_create(
-                {"model": ckan.model, "ignore_auth": True, "user": username}, data_dict)
-
-            # Update the user-facing log
-            log_writer.info("Added new resource to dataset "
-                            + config.get('ckan.site_url')
-                            + helpers.url_for(controller='package',
-                                              action='resource_read',
-                                              id=new_resource['package_id'],
-                                              resource_id=new_resource['id'])
-                            )
-
-            log_writer.info("Location mapping completed", state="complete")
-        except LookupNameMissingException:
-            log_writer.error("The location column '{}' wasn't found in the resource file".format(column_name), state="Something went wrong")
+    except LookupNameMissingException:
+        '''
+        LocationMapper raises this exception if the location column isn't found, so we mirror
+        that into the user-facing task log.
+        '''
+        log_writer.error("The location column '{}' wasn't found in the resource file".format(column_name), state="Something went wrong")
 
     except Exception as exception:
         '''
@@ -89,3 +76,29 @@ def perform(resource_id, column_name, column_type, is_name, username):
         log_writer.error("An error occurred in the code! Please check the system logs.", state="Something went wrong")
         logging.exception(exception)
         pass
+
+def __read_csv_from_resource(resource):
+    resource_path = uploader.get_resource_uploader(resource).get_path(resource['id'])
+    resource_contents = codecs.open(resource_path, 'rb', 'cp1257')
+
+    return pandas.read_csv(resource_contents)
+
+def __create_and_upload_geojson_resource(package_id, name, description, filename, geojson_obj, username):
+    file_buffer = StringIO()
+    geojson.dump(geojson_obj, file_buffer, ignore_nan=True)
+
+    upload = cgi.FieldStorage()
+    upload.filename = filename
+    upload.file = file_buffer
+
+    data_dict = {
+        "package_id": package_id,
+        "name": name,
+        "description": description,
+        "format": "application/geo+json",
+        "upload": upload
+    }
+
+    return logic.action.create.resource_create({"model": ckan.model,
+                                                "ignore_auth": True,
+                                                "user": username}, data_dict)
