df2fc0440626972ded24fec4818c44123c22e7a0
Ryan Brooks <ryan@slatehorse.com>
2018-01-30 12:36:09 +0000

    WIP; works but has some desperate need for TLC and a hot chicken dinner

--- ckanext/string_to_location/controller.py
+++ ckanext/string_to_location/controller.py
@@ -6,12 +6,12 @@ from ckan.controllers.package import PackageController
 import ckan.lib.helpers as helpers
 import ckan.lib.uploader as uploader
 from ckan.lib.navl.validators import not_empty
-from ckan.common import c
-from ckan.common import config
-import ckan.model
-import ckan.logic as logic
+import ckan.plugins.toolkit
+import ckan.common
 
+from ckanext.string_to_location.location_mapper import map_location_async
 from ckanext.string_to_location.location_mapper import LocationMapper
+
 from ckanext.string_to_location.location_mapper_log_reader import LocationMapperLogReader
 from ckanext.string_to_location.location_mapper_log_writer import LocationMapperLogWriter
 
@@ -20,44 +20,37 @@ import os
 import sys
 import codecs
 import csv
-import geojson
-import pandas
 import ast
-
 
 class LocationMapperController(PackageController):
 
     def map_location(self, id, resource_id):
         context = None
-        resource = toolkit.get_action('resource_show')(
-            context, {'id': resource_id})
-        log_writer = LocationMapperLogWriter(resource['id']) 
+        resource = toolkit.get_action('resource_show')(context, {'id': resource_id})
 
-        column_name, column_type, is_name = self._set_column_name_and_type(resource)
+        # Validate the resource before pushing the job
+        log_writer = LocationMapperLogWriter(resource['id'])
+
+        if 'location_column' in resource and 'location_type' in resource:
+            column_name = resource['location_column']
+            column_type=resource['location_type']
+            is_name = resource['location_type'].endswith('_name')
+        elif 'location_column' in resource['_extras'] and 'location_type' in resource['_extras']:
+            extras = ast.literal_eval(resource['_extras'])
+            column_name = extras['location_column']
+            column_type=extras['location_type']
+            is_name = extras['location_type'].endswith('_name')
+        else:
+            column_name = None
+            column_type = None
+            is_name = None
 
         if column_name is None:
             log_writer.error("The resource does not specify location columns", state="Something went wrong")
-            return helpers.redirect_to(controller='ckanext.string_to_location.controller:LocationMapperController',
-                                   action='resource_location_mapping_status', id=id, resource_id=resource_id)
-
-        resource_path = uploader.get_resource_uploader(resource).get_path(resource['id'])
-        resource_contents = codecs.open(resource_path, 'rb', 'cp1257')
-
-        table = pandas.read_csv(resource_contents)
-
-        log_writer.info("Read " + resource['name'] + " in")
-
-        output_buffer = LocationMapper(table, column_name, column_type, is_name).map_location()
-
-        new_resource = self._upload_augmented_resource(resource, output_buffer)
-
-        log_writer.info("Added new resource to dataset " \
-                       + config.get('ckan.site_url')  \
-                       + helpers.url_for(controller='package', 
-                                        action='resource_read', 
-                                        id=new_resource['package_id'], 
-                                        resource_id=new_resource['id']), 
-                       state="complete")
+        else:
+            # Enqueue the location_mapping task
+            ckan.plugins.toolkit.enqueue_job(map_location_async, [], {u'resource_id': resource['id'], u'username': ckan.common.c.userobj.name}, title='map_location_async')
+            log_writer.info("Queued location mapping job TODO_ID")
 
         return helpers.redirect_to(controller='ckanext.string_to_location.controller:LocationMapperController',
                                    action='resource_location_mapping_status', id=id, resource_id=resource_id)
@@ -83,39 +76,3 @@ class LocationMapperController(PackageController):
         return render('custom/resource_location_mapping_status.html',
                            extra_vars={'status': status})
 
-    def _set_column_name_and_type(self, resource):
-
-        if 'location_column' in resource and 'location_type' in resource:
-            column_name = resource['location_column']
-            column_type=resource['location_type']
-            is_name = resource['location_type'].endswith('_name')
-        elif 'location_column' in resource['_extras'] and 'location_type' in resource['_extras']:
-            extras = ast.literal_eval(resource['_extras'])
-            column_name = extras['location_column']
-            column_type=extras['location_type']
-            is_name = extras['location_type'].endswith('_name')
-        else:
-            column_name = None
-            column_type = None
-            is_name = None
-
-        return column_name, column_type, is_name
-
-    def _upload_augmented_resource(self, resource, output_buffer):
-        upload = cgi.FieldStorage()
-        # FIXME: replace with source filename + modified extension
-        upload.filename = 'mapped_output.geojson'
-        upload.file = output_buffer
-
-        data_dict = {
-            "package_id": resource['package_id'],
-            "name": "Augmented " + resource['name'],
-            "description": "Geo-mapped representation of " + resource['name'],
-            "format": "application/geo+json",
-            "upload": upload
-        }
-
-        new_resource = logic.action.create.resource_create(
-            {"model": ckan.model, "user": c.userobj.name}, data_dict)
-
-        return new_resource
--- ckanext/string_to_location/location_mapper.py
+++ ckanext/string_to_location/location_mapper.py
@@ -4,15 +4,96 @@ import sys
 import codecs
 import csv
 import geojson
+import logging
+import pandas
 
 from geojson import Feature, FeatureCollection
 from StringIO import StringIO
+
+import ckan.plugins.toolkit as toolkit
+import ckan.lib.uploader as uploader
+import ckan.logic as logic
+from ckan.common import config
+
+import ckan.lib.helpers as helpers
+import ckan.model
+
+from ckanext.string_to_location.location_mapper_log_reader import LocationMapperLogReader
+from ckanext.string_to_location.location_mapper_log_writer import LocationMapperLogWriter
 
 from ckanext.string_to_location.ons_entity_types import OnsEntityTypes
 from ckanext.string_to_location.null_ons_entity import NullOnsEntity
 from ckanext.string_to_location.ons_entity_builder import OnsEntityBuilder
 from ckanext.string_to_location.ons_code_mapper import OnsCodeMapper
 
+# FIXME comments EVIL HACK
+def map_location_async(resource_id, username):
+    '''
+    Background job
+    '''
+
+    context = None
+    resource = toolkit.get_action('resource_show')(context, {'id': resource_id})
+
+    log_writer = LocationMapperLogWriter(resource['id'])
+
+    #
+    # Validation
+    # TODO: This is carried out in the controller, so we need to either:
+    #   1. Trust that the input is good...
+    #   2. Refactor this into a common place to keep it DRY
+    #
+    if 'location_column' in resource and 'location_type' in resource:
+        column_name = resource['location_column']
+        column_type=resource['location_type']
+        is_name = resource['location_type'].endswith('_name')
+    elif 'location_column' in resource['_extras'] and 'location_type' in resource['_extras']:
+        extras = ast.literal_eval(resource['_extras'])
+        column_name = extras['location_column']
+        column_type=extras['location_type']
+        is_name = extras['location_type'].endswith('_name')
+    else:
+        column_name = None
+        column_type = None
+        is_name = None
+
+    if column_name is None:
+        log_writer.error("The resource does not specify location columns", state="Something went wrong")
+
+
+    resource_path = uploader.get_resource_uploader(resource).get_path(resource['id'])
+    resource_contents = codecs.open(resource_path, 'rb', 'cp1257')
+
+    table = pandas.read_csv(resource_contents)
+
+    log_writer.info("Loaded contents of " + resource['name'])
+
+    output_buffer = LocationMapper(table, column_name, column_type, is_name).map_location()
+
+    upload = cgi.FieldStorage()
+    # FIXME: replace with source filename + modified extension
+    upload.filename = 'mapped_output.geojson'
+    upload.file = output_buffer
+
+    data_dict = {
+        "package_id": resource['package_id'],
+        "name": "Augmented " + resource['name'],
+        "description": "Geo-mapped representation of " + resource['name'],
+        "format": "application/geo+json",
+        "upload": upload
+    }
+
+    new_resource = logic.action.create.resource_create(
+        {"model": ckan.model, "ignore_auth": True, "user": username}, data_dict)
+
+    # Update the user-facing log
+    log_writer.info("Added new resource to dataset " \
+                   + config.get('ckan.site_url')  \
+                   + helpers.url_for(controller='package',
+                                    action='resource_read',
+                                    id=new_resource['package_id'],
+                                    resource_id=new_resource['id']),
+                   state="complete")
 
 class LocationMapper:
 
@@ -28,10 +109,10 @@ class LocationMapper:
         self.is_name = is_name
 
     def map_location(self):
-                       
+
         source_entity_type = self.COLUMN_TYPE_TO_ENTITY_TYPE[self.column_type]
         target_entity_type = OnsEntityTypes.LOCAL_AUTHORITY_DISTRICT
-       
+
         matches, errors = self._build_matches(self.table, self.column_name, self.is_name, source_entity_type, target_entity_type)
 
         geojson_version = self._matches_to_geojson(matches, list(self.table.columns))
@@ -39,7 +120,7 @@ class LocationMapper:
         output_buffer = StringIO()
         geojson.dump(geojson_version, output_buffer, ignore_nan=True)
 
-        return output_buffer        
+        return output_buffer
 
         #
         # Summary info
@@ -58,7 +139,7 @@ class LocationMapper:
         # print(f"    {error_count} errors")
         # print("")
         # print(f"    {matches_with_polygons} matches with polygons")
-        
+
 
     def _matches_to_geojson(self, match_array, properties):
             features = []
@@ -106,4 +187,4 @@ class LocationMapper:
 
         return matches, errors
 
-    
+
